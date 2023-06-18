3b5f4c5a3e77634c46f17133d584503e279b0769
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-06-18 23:37:04 +0530

    Make token burnable

--- backend/contracts/SSToken.sol
+++ backend/contracts/SSToken.sol
@@ -3,26 +3,37 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract SocialScoreToken is ERC20 {
-    uint public tokenPrice;  // Price of each token in wei
+contract SocialScoreToken is ERC20, ERC20Burnable {
+    uint256 public tokenPrice; // Price of each token in wei
 
     mapping(address => bool) public adminList;
+    mapping(address => uint256) private memberBalance;
 
-    constructor(uint _tokenPrice) ERC20("Social Score Token", "SS") {
+    constructor(uint256 _tokenPrice) ERC20("Social Score Token", "SS") {
         tokenPrice = _tokenPrice;
         adminList[msg.sender] = true;
     }
 
-    modifier onlyAdmin {
+    modifier onlyAdmin() {
         require(adminList[msg.sender] == true, "Don't have access!!");
 
         _;
     }
 
     function buyTokens() external payable {
-        uint tokenAmount = msg.value / tokenPrice;  // Calculate the amount of tokens based on ETH sent
-        _mint(msg.sender, tokenAmount);  // Mint the purchased tokens and assign them to the buyer
+        uint256 tokenAmount = msg.value / tokenPrice; // Calculate the amount of tokens based on ETH sent
+        _mint(msg.sender, tokenAmount); // Mint the purchased tokens and assign them to the buyer
+
+        memberBalance[msg.sender] += tokenAmount;
+    }
+
+    function spendToken(uint256 _amount) public {
+        require(_amount <= memberBalance[msg.sender], "Low balance!!");
+        memberBalance[msg.sender] -= _amount;
+
+        burn(_amount);
     }
 
     function addAdmin(address _address) public onlyAdmin {
